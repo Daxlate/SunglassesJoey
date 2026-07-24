556527c4b5bf28df4860b69ee9f36f07d906a8f9
Sara <86428500+SARA02MASSIEL@users.noreply.github.com>
2026-07-24 19:26:36 -0400

    BasicUpgradesthingy

--- upgrade_1.gd
+++ upgrade_1.gd
@@ -1,1 +1,17 @@
 extends TextureButton
+var Upgrade = 1
+var Title = ""
+var Desc = ""
+var lines = []
+
+func _ready():
+	var file = FileAccess.open("res://UpgradesText.txt", FileAccess.READ)
+	while not file.eof_reached():
+		lines.append(file.get_line())
+	$UpgradeTitle1.text = read_specific_line(Upgrade)
+
+func read_specific_line(line_index: int):
+	var tit = "ERROR"
+	if line_index < lines.size():
+		tit = (lines[line_index])
+	return tit

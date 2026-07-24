a0855690d6db51735c03a343fa2e01076112252b
Sara <86428500+SARA02MASSIEL@users.noreply.github.com>
2026-07-24 01:28:45 -0400

    Timer Working

--- scenes/pentagram.gd
+++ scenes/pentagram.gd
@@ -3,6 +3,13 @@ var State = 0
 
 func _process(delta):
 	%PentagramFloor.position = global_position
+	var OverlappingBody = get_overlapping_bodies()
+	if OverlappingBody.size() > 0:
+		if (Input.is_action_pressed("shoot") && State == 1):
+			TurnOff()
+			$Timer.start()
+			$Label.text = "asd"
+		pass
 
 func _on_timer_timeout():
 	$Label.text = "yippe"
@@ -15,10 +22,11 @@ func TurnOn():
 	State = 1
 	%PentagramFloor.play("LightUp")
 	%ExtraEffects.play("Animation")
-func _on_pentagram_floor_animation_finished():
-	%PentagramFloor.play("On")
-	$Label.text = "sauce"
 
+func _on_pentagram_floor_animation_finished():
+	if State == 1:
+		%PentagramFloor.play("On")
+		$Label.text = "sauce"
 
 func _on_extra_effects_animation_finished():
 	%ExtraEffects.play("Off")

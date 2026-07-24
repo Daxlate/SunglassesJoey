ed335d605dd800569b1d48cb62c115b53e9f0dbb
Daxlate <86430483+Daxlate@users.noreply.github.com>
2026-07-24 00:00:21 -0400

    fps change and other stuff

--- JoeyMovement.gd
+++ JoeyMovement.gd
@@ -7,7 +7,7 @@ func _process(delta):
 	var usable_angle = rad_to_deg(angle_to_mouse)
 	var final_vector = Vector2.from_angle(angle_to_mouse) * 50   
 	$Crosshair.position = final_vector
-	$Label.text = str(is_punch)
+	
 	
 	
 func _physics_process(delta):

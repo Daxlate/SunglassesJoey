ae1a7c79c8491cd6e85b21ae0ce1b2da367a911a
Daxlate <86430483+Daxlate@users.noreply.github.com>
2026-08-09 01:23:04 -0400

    Wave type and wave diffs

--- scripts/Maincontrol.gd
+++ scripts/Maincontrol.gd
@@ -4,20 +4,25 @@ var Spawn_cap := 20
 var Spawn_amount := 0
 var Spawn_multiplier := 1.5
 var Timer_multiplier := 0.95
+var wave_number := 1
 var Current_preset
+var wave_difficulty = 1
 
 @export var Wavetimer: Timer
 @export var testlabel: Label
 
 func spawner():
 	
-	if Current_preset == 1:
-		testpreset()
+	match(Current_preset):
+	
+		1:preset_melee()
+		2:preset_mixed()
+		3:preset_mixed()
 		
 	if Current_preset == null:
-		preset_choose()
-		Current_preset = preset_choose()
-		print(Current_preset)
+		Current_preset = 1
+	
+	
 	
 func spawnmobmelee():
 	var basic_goon_scene = preload("res://scenes/goons/basic_goon.tscn")
@@ -36,13 +41,23 @@ func spawnmobranged():
 
 
 func _on_wave_spawn_timer_timeout() -> void:
+	
+	if wave_number > 10:
+		wave_number = 1
+		wave_difficulty = 1
+		
 	if Spawn_amount == Spawn_cap:
+		match (wave_number):
+			3: wave_difficulty = 2
+			6: wave_difficulty = 3
+			10: wave_difficulty = 3
 		Spawn_amount = 0
 		Spawn_cap = Spawn_cap * Spawn_multiplier
 		Wavetimer.wait_time = Wavetimer.wait_time * Timer_multiplier
 		Current_preset = preset_choose()
+		wave_number += 1
 		
-	testlabel.text = str(Spawn_cap) + " " + str(Wavetimer.wait_time)
+	testlabel.text = str(Spawn_cap) + " " + str(Wavetimer.wait_time) + " " + str(wave_number) + " " + str(Current_preset) + " " + str(wave_difficulty)
 	spawner()
 	Spawn_amount += 1
 	
@@ -53,7 +68,61 @@ func testpreset():
 		spawnmobmelee()
 	else:
 		spawnmobranged()
+
+func preset_melee():
+	
+	match(wave_difficulty):
+		1:
+			spawnmobmelee()
+		2:
+			spawnmobmelee()
+		3:
+			spawnmobmelee()
+
+
+func preset_mixed():
+	match(wave_difficulty):
+		1:
+			var rand_range = randi_range(1,10)
+			if rand_range < 8:
+				spawnmobmelee()
+			else:
+				spawnmobranged()
+		2:
+			var rand_range = randi_range(1,10)
+			if rand_range < 8:
+				spawnmobmelee()
+			else:
+				spawnmobranged()
+		3:
+			var rand_range = randi_range(1,10)
+			if rand_range < 8:
+				spawnmobmelee()
+			else:
+				spawnmobranged()
+				
+func preset_rangedmixed():
+	match(wave_difficulty):
+		1:
+			var rand_range = randi_range(1,10)
+			if rand_range > 8:
+				spawnmobmelee()
+			else:
+				spawnmobranged()
+		2:
+			var rand_range = randi_range(1,10)
+			if rand_range > 8:
+				spawnmobmelee()
+			else:
+				spawnmobranged()
+		3:
+			var rand_range = randi_range(1,10)
+			if rand_range > 8:
+				spawnmobmelee()
+			else:
+				spawnmobranged()
+
 	
 func preset_choose():
-	var rand_range = randi_range(1,1)
+	var rand_range = randi_range(1,3)
 	return rand_range
